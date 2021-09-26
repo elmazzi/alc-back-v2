2085ec4a8a19e0bf066b975c783654d4226bb749
kati hajar <hakati1999@hotmail.com>
2021-09-26 23:57:00 +0100

    Commit

--- src/main/java/ma/learn/quiz/rest/prof/ProfReviewProfRest.java
+++ src/main/java/ma/learn/quiz/rest/prof/ProfReviewProfRest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/Prof/profReview")
+@RequestMapping("/prof/profReview")
 public class ProfReviewProfRest {
     @GetMapping("/review/{review}")
     public ProfReview findByReview(@PathVariable int review) {
