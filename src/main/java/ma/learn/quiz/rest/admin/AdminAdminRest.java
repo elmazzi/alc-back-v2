05bf973157414e701c93f69f23befae3e8f39a6a
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2022-01-29 22:02:28 +0100

    commit-youssef: Add image to google drive

--- src/main/java/ma/learn/quiz/rest/admin/AdminAdminRest.java
+++ src/main/java/ma/learn/quiz/rest/admin/AdminAdminRest.java
@@ -1,10 +1,32 @@
 package ma.learn.quiz.rest.admin;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
 import ma.learn.quiz.bean.Admin;
 import ma.learn.quiz.service.AdminService;
 import miniApp.migration.DataBaseMigration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -39,14 +61,96 @@ public class AdminAdminRest {
     public Object findByCritere(@PathVariable String login, @PathVariable String password) {
         return adminService.findByCritere(login, password);
     }
+
     @Autowired
     private DataBaseMigration dataBaseMigration;
 
 
     @GetMapping("/app")
-    public void start() {
+    public void start() throws Exception {
         dataBaseMigration.htmlimagetext();
     }
+
+    @GetMapping("/app/saveimage")
+    public void createFile() throws Exception {
+        GoogleClientSecrets secrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(gdSecretKeys.getInputStream()));
+        flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, secrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(credentialsFolder.getFile())).build();
+
+
+        Credential cred = this.flow.loadCredential(USER_IDENTIFIER_KEY);
+
+        Drive drive = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, cred).setApplicationName(APP_NAME).build();
+        com.google.api.services.drive.model.File file = new com.google.api.services.drive.model.File();
+        com.google.api.services.drive.model.File folder = new com.google.api.services.drive.model.File();
+        com.google.api.services.drive.model.File folder2 = new com.google.api.services.drive.model.File();
+
+// -------------------------------- FOLDER ONE ----------------------------------------
+        folder2.setName("folder1");
+        folder2.setMimeType("application/vnd.google-apps.folder");
+        com.google.api.services.drive.model.File uploadedFolder2 = drive.files().create(folder2).setFields("id").execute();
+        // -------------------------------- FOLDER TWO ----------------------------------------
+
+        folder.setName("folder");
+        folder.setMimeType("application/vnd.google-apps.folder");
+        folder.setParents(Arrays.asList(uploadedFolder2.getId()));
+        com.google.api.services.drive.model.File uploadedFolder = drive.files().create(folder).setFields("id").execute();
+
+
+        file.setName("1.jpg");
+        file.setParents(Arrays.asList(uploadedFolder.getId()));
+        FileContent content = new FileContent("image/jpeg", new java.io.File("C:\\KamImages\\images\\parcours2\\Lesson\\DISCUSSION1\\1.jpg"));
+        com.google.api.services.drive.model.File uploadedFile = drive.files().create(file, content).setFields("id").execute();
+        String fileReference = String.format("{fileID: '%s'}", uploadedFile.getId());
+        System.out.println("=========================================================");
+        System.out.println(fileReference);
+    }
+
+
+
+
+    @GetMapping("/googlesignin")
+    public void googleSignIn(HttpServletResponse response) throws Exception {
+        this.generateFlow();
+        GoogleAuthorizationCodeRequestUrl url = flow.newAuthorizationUrl();
+        String redirectUrl = url.setRedirectUri(CALLBACK_URL).setAccessType("offline").build();
+        response.sendRedirect(redirectUrl);
+    }
+
+    @GetMapping("/oauth")
+    public void saveAuthorizationCode(HttpServletRequest request) throws Exception {
+        String code = request.getParameter("code");
+        if (code != null) {
+            saveToken(code);
+        }
+    }
+
+    public void generateFlow() throws Exception {
+        GoogleClientSecrets secrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(gdSecretKeys.getInputStream()));
+        flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, secrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(credentialsFolder.getFile())).build();
+    }
+
+    private void saveToken(String code) throws Exception {
+        GoogleTokenResponse response = flow.newTokenRequest(code).setRedirectUri(CALLBACK_URL).execute();
+        flow.createAndStoreCredential(response, USER_IDENTIFIER_KEY);
+    }
+
+
+    private static final String APP_NAME = "alc-project-drive-img";
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
+    private static final String USER_IDENTIFIER_KEY = "526671645204-pefjgst2s07uo5k8v0srh75pc7i28e8p.apps.googleusercontent.com";
+    private GoogleAuthorizationCodeFlow flow;
+
     @Autowired
     private AdminService adminService;
+
+    @Value("${google.oauth.callback.uri}")
+    private String CALLBACK_URL;
+    @Value("${google.secret.key.path}")
+    private Resource gdSecretKeys;
+    @Value("${google.credentials.folder.path}")
+    private Resource credentialsFolder;
 }
--- src/main/java/miniApp/migration/DataBaseMigration.java
+++ src/main/java/miniApp/migration/DataBaseMigration.java
@@ -5,20 +5,45 @@
  */
 package miniApp.migration;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.FileList;
+import com.google.api.services.drive.model.Permission;
 import ma.learn.quiz.bean.*;
 import ma.learn.quiz.dao.*;
+import ma.learn.quiz.service.AdminService;
 import miniApp.migration.constant.Constants;
 import miniApp.migration.util.FileUtil;
 import miniApp.migration.util.DownloaderUtil;
 import miniApp.migration.util.JsoupUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
 import javax.persistence.Id;
+import javax.persistence.SynchronizationType;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 @Service
 public class DataBaseMigration {
     public DataBaseMigration() {
@@ -38,7 +63,7 @@ public class DataBaseMigration {
     private SuperCategorieSectionDao superCategorieSectionDao;
     private Long id;
 
-    public void htmlimagetext() {
+    public void htmlimagetext() throws Exception {
         File imageDirRoot = FileUtil.mkdire(Constants.root1, "images", true);
         File directoryPathParcous = new File(Constants.root);
         String[] parcour = directoryPathParcous.list();
@@ -55,23 +80,23 @@ public class DataBaseMigration {
                 }
                 String pathParcoursImages = imageDirRoot.getAbsolutePath() + "\\" + parcour[i] + "\\";
                 String pathParcours = Constants.root + parcour[i] + "\\";
-                    FileUtil.mkdire(pathParcoursImages, "Lesson", true);
-                    String pathLessonOrHomeWork = pathParcours + "Lesson";
-                    String pathLessonOrHomeWorkImage = pathParcoursImages + "Lesson";
-                    File directoryPathSection = new File(pathLessonOrHomeWork);
-                    String[] section = directoryPathSection.list();
-                    for (int j = 0; j < section.length; j++) {
-                        System.out.println("  sectionName ::::: " + "Lesson" + " " + parcour[i] + " " + section[j]);
-                        String pathSection = pathLessonOrHomeWork + "\\" + section[j];
-                        String pathSectionImage = pathLessonOrHomeWorkImage + "\\" + section[j];
-                        if (new File(pathSection).exists()) {
-                            FileUtil.mkdire(pathSectionImage, pathLessonOrHomeWorkImage, true);
-                            System.out.println("pathSection ==> " + pathSection);
-                            System.out.println("pathImage ==>" + pathSectionImage);
-                            System.out.println("++++++++++++++++++++++++++++++");
-                            extractHtmlImageAndContent(parcour[i], section[j], pathSection, pathSectionImage);
-                        }
+                FileUtil.mkdire(pathParcoursImages, "Lesson", true);
+                String pathLessonOrHomeWork = pathParcours + "Lesson";
+                String pathLessonOrHomeWorkImage = pathParcoursImages + "Lesson";
+                File directoryPathSection = new File(pathLessonOrHomeWork);
+                String[] section = directoryPathSection.list();
+                for (int j = 0; j < section.length; j++) {
+                    System.out.println("  sectionName ::::: " + "Lesson" + " " + parcour[i] + " " + section[j]);
+                    String pathSection = pathLessonOrHomeWork + "\\" + section[j];
+                    String pathSectionImage = pathLessonOrHomeWorkImage + "\\" + section[j];
+                    if (new File(pathSection).exists()) {
+                        FileUtil.mkdire(pathSectionImage, pathLessonOrHomeWorkImage, true);
+                        System.out.println("pathSection ==> " + pathSection);
+                        System.out.println("pathImage ==>" + pathSectionImage);
+                        System.out.println("++++++++++++++++++++++++++++++");
+                        extractHtmlImageAndContent(parcour[i], section[j], pathSection, pathSectionImage);
                     }
+                }
 
 
             }
@@ -82,6 +107,7 @@ public class DataBaseMigration {
 
     public void extractHtmlImageAndContent(String nameparcours, String categorieSection, String directoryName, String imagePath) {
         List<File> htmlFiles = FileUtil.findHtmlFiles(directoryName);
+        AtomicInteger index = new AtomicInteger();
 
         htmlFiles.stream()
                 .sorted((f1, f2) -> FileUtil.compare(f1, f2))
@@ -123,6 +149,12 @@ public class DataBaseMigration {
                                 cours = new Cours();
                                 cours.setParcours(parcours);
                                 cours.setLibelle(JsoupUtil.getElementContent(f, "div.link-dropdown_link-dropdown"));
+                                if (Objects.equals(cours.getLibelle(), "Intro lesson")) {
+                                    cours.setNumeroOrder(0);
+                                } else {
+                                    String myTxt = cours.getLibelle().substring(0, 2).replaceAll("\\s", "");
+                                    cours.setNumeroOrder(Integer.parseInt(myTxt));
+                                }
                                 coursDao.save(cours);
                                 parcours.setNombreCours(parcours.getNombreCours() + 1);
                                 parcoursDao.save(parcours);
@@ -151,12 +183,23 @@ public class DataBaseMigration {
                             }
                             final String imageNameDestination = imagePath + "\\" + FileUtil.fileNameWithOutExt(f.getName()) + "." + fileExtention;
                             System.out.println("imageNameSource = " + imageNameSource + " && imageNameDestination " + imageNameDestination);
+                            System.out.println(parcours.getLibelle());
+                            System.out.println("Lesson");
+                            System.out.println(section.getCategorieSection().getLibelle());
                             FileUtil.copyFile(imageNameSource, imageNameDestination);
                             System.out.println("***********************************\n");
+                            String imgName = FileUtil.fileNameWithOutExt(f.getName()) + "." + fileExtention;
+
+                            String drivePath = this.createFile(parcours.getLibelle(),section.getCategorieSection().getLibelle(),imageNameDestination, imgName);
+                            System.out.println("_______________________________________________________________________");
+                            System.out.println(drivePath);
+                            section.setUrlImage(drivePath);
+                            sectionDao.save(section);
+                            System.out.println("_______________________________________________________________________");
                         }
 
 
-                    } catch (IOException ex) {
+                    } catch (Exception ex) {
                         Logger.getLogger(DataBaseMigration.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 });
@@ -170,4 +213,99 @@ public class DataBaseMigration {
     public Long getId() {
         return id;
     }
+    public String createFile(String parcours, String section, String imgPath, String imgName ) throws Exception {
+        boolean isParcourFolderExist = false;
+        boolean isSectionFolderExist = false;
+        boolean isImgExist = false;
+        GoogleClientSecrets secrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(gdSecretKeys.getInputStream()));
+        flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, secrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(credentialsFolder.getFile())).build();
+
+
+        Credential cred = this.flow.loadCredential(USER_IDENTIFIER_KEY);
+        Permission permission = new Permission();
+        permission.setType("anyone");
+        permission.setRole("reader");
+
+        Drive drive = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, cred).setApplicationName(APP_NAME).build();
+        com.google.api.services.drive.model.File file = new com.google.api.services.drive.model.File();
+        com.google.api.services.drive.model.File folder = new com.google.api.services.drive.model.File();
+        com.google.api.services.drive.model.File folder2 = new com.google.api.services.drive.model.File();
+
+
+        com.google.api.services.drive.model.File uploadedFolder2 = new com.google.api.services.drive.model.File();
+        com.google.api.services.drive.model.File uploadedFolder = new com.google.api.services.drive.model.File();
+        com.google.api.services.drive.model.File uploadedFile = new com.google.api.services.drive.model.File();
+// -------------------------------- FOLDER PARCOURS ----------------------------------------
+        FileList sheckFile = drive.files().list().setFields("files(id,name,thumbnailLink)").execute();
+
+        for (com.google.api.services.drive.model.File f: sheckFile.getFiles()
+             ) {
+            System.out.println("=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=");
+            System.out.println(f.getThumbnailLink());
+            System.out.println(f.getId());
+            System.out.println(f.getName());
+            System.out.println("=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=");
+            if (Objects.equals(parcours, f.getName())){
+                isParcourFolderExist = true;
+                uploadedFolder2 = f;
+            }
+
+            if (Objects.equals(section, f.getName())){
+                 isSectionFolderExist = true;
+                uploadedFolder = f;
+            }
+
+            if (Objects.equals(imgName, f.getName())){
+                 isImgExist = true;
+                uploadedFile = f;
+            }
+
+        }
+        if (!isParcourFolderExist){
+
+            folder2.setName(parcours);
+            folder2.setMimeType("application/vnd.google-apps.folder");
+             uploadedFolder2 = drive.files().create(folder2).setFields("id").execute();
+            drive.permissions().create(uploadedFolder2.getId(), permission).execute();
+
+        }
+
+        if (!isSectionFolderExist){
+            folder.setName(section);
+            folder.setMimeType("application/vnd.google-apps.folder");
+            folder.setParents(Arrays.asList(uploadedFolder2.getId()));
+            uploadedFolder = drive.files().create(folder).setFields("id").execute();
+            drive.permissions().create(uploadedFolder.getId(), permission).execute();
+        }
+
+        if (!isImgExist){
+            file.setName(imgName);
+            file.setParents(Arrays.asList(uploadedFolder.getId()));
+            FileContent content = new FileContent("image/jpeg", new java.io.File(imgPath));
+            uploadedFile = drive.files().create(file, content).setFields("id,thumbnailLink").execute();
+            drive.permissions().create(uploadedFile.getId(), permission).execute();
+        }
+        System.out.println("==============================ID THUMBNAILLINK==============================================");
+        System.out.println(uploadedFile.getId());
+        System.out.println(uploadedFile.getThumbnailLink());
+        System.out.println("============================================================================");
+        return uploadedFile.getThumbnailLink();
+    }
+
+
+    private static final String APP_NAME = "alc-project-drive-img";
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
+    private static final String USER_IDENTIFIER_KEY = "526671645204-pefjgst2s07uo5k8v0srh75pc7i28e8p.apps.googleusercontent.com";
+    private GoogleAuthorizationCodeFlow flow;
+
+    @Value("${google.oauth.callback.uri}")
+    private String CALLBACK_URL;
+    @Value("${google.secret.key.path}")
+    private org.springframework.core.io.Resource gdSecretKeys;
+    @Value("${google.credentials.folder.path}")
+    private Resource credentialsFolder;
+
 }
