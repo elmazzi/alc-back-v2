d2fb1b270dc81a9af5e5d9800252029022de01e1
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2022-03-06 21:55:35 +0100

    commit-youssef: Quiz review Admin

--- src/main/java/ma/learn/quiz/dao/QuizDao.java
+++ src/main/java/ma/learn/quiz/dao/QuizDao.java
@@ -5,12 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ma.learn.quiz.bean.Quiz;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Repository
 public interface QuizDao extends JpaRepository<Quiz, Long> {
     Quiz findByRef(String ref);
     Quiz findQuizById(Long id);
+    @Transactional
     int deleteByRef(String ref);
     Quiz findBySectionId(Long id);
 }
