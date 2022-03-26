0cf3da88ee76c997bbe39dab9c3607260532736c
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2022-03-26 14:45:20 +0100

    commit-youssef: quiz drag and drop

--- src/main/java/ma/learn/quiz/service/ReponseEtudiantService.java
+++ src/main/java/ma/learn/quiz/service/ReponseEtudiantService.java
@@ -17,87 +17,91 @@ import ma.learn.quiz.bean.ReponseEtudiant;
 import ma.learn.quiz.dao.ReponseEtudiantDao;
 
 
-
 @Service
 public class ReponseEtudiantService {
 
-	@Transactional
-	public int deleteByQuizEtudiantId(Long id) {
-		return reponseEtudiantDao.deleteByQuizEtudiantId(id);
-	}
+    @Transactional
+    public int deleteByQuizEtudiantId(Long id) {
+        return reponseEtudiantDao.deleteByQuizEtudiantId(id);
+    }
 
-	@Autowired
-	private ReponseEtudiantDao reponseEtudiantDao;
+    @Autowired
+    private ReponseEtudiantDao reponseEtudiantDao;
 
-	@Autowired
+    @Autowired
     private EntityManager entityManager;
-	
-	public List<ReponseEtudiant> findByCriteria(Long idQuizEtudiant, Long idQuestion) {
-		String query = "SELECT r FROM ReponseEtudiant r WHERE r.quizEtudiant.id= '"+idQuizEtudiant+"' and r.question.id='"+idQuestion+"'";
-		return entityManager.createQuery(query).getResultList();
-	}
 
-	public List<ReponseEtudiant> findByQuizEtudiantRef(String ref) {
-		return reponseEtudiantDao.findByQuizEtudiantRef(ref);
-	}
+    public List<ReponseEtudiant> findByCriteria(Long idQuizEtudiant, Long idQuestion) {
+        String query = "SELECT r FROM ReponseEtudiant r WHERE r.quizEtudiant.id= '" + idQuizEtudiant + "' and r.question.id='" + idQuestion + "'";
+        return entityManager.createQuery(query).getResultList();
+    }
 
-	@Autowired
-	private QuizEtudiantService quizEtudiantService;
-	@Autowired
-	private QuestionService questionService;
-	@Autowired
-	private ReponseService reponseService;
-	@Autowired
-	private EtudiantService etudiantService;
+    public List<ReponseEtudiant> findByQuizEtudiantRef(String ref) {
+        return reponseEtudiantDao.findByQuizEtudiantRef(ref);
+    }
 
-	public ReponseEtudiant findByRef(String ref) {
-		return reponseEtudiantDao.findByRef(ref);
-	}
+    @Autowired
+    private QuizEtudiantService quizEtudiantService;
+    @Autowired
+    private QuestionService questionService;
+    @Autowired
+    private ReponseService reponseService;
+    @Autowired
+    private EtudiantService etudiantService;
 
-	@Transactional
-	public int deleteByRef(String Ref) {
-		return reponseEtudiantDao.deleteByRef(Ref);
-	}
+    public ReponseEtudiant findByRef(String ref) {
+        return reponseEtudiantDao.findByRef(ref);
+    }
 
-	public List<ReponseEtudiant> findByReponseQuestionRef(String ref) {
-		return reponseEtudiantDao.findByReponseQuestionRef(ref);
-	}
+    @Transactional
+    public int deleteByRef(String Ref) {
+        return reponseEtudiantDao.deleteByRef(Ref);
+    }
 
-	public List<ReponseEtudiant> findByQuizEtudiantEtudiantRef(String ref) {
-		return reponseEtudiantDao.findByQuizEtudiantEtudiantRef(ref);
-	}
+    public List<ReponseEtudiant> findByReponseQuestionRef(String ref) {
+        return reponseEtudiantDao.findByReponseQuestionRef(ref);
+    }
 
-	@Transactional
-	public int deleteByReponseQuestionRef(String ref) {
-		return reponseEtudiantDao.deleteByReponseQuestionRef(ref);
-	}
+    public List<ReponseEtudiant> findByQuizEtudiantEtudiantRef(String ref) {
+        return reponseEtudiantDao.findByQuizEtudiantEtudiantRef(ref);
+    }
 
-	@Transactional
-	public int deleteByQuizEtudiantEtudiantRef(String ref) {
-		return reponseEtudiantDao.deleteByQuizEtudiantEtudiantRef(ref);
-	}
+    @Transactional
+    public int deleteByReponseQuestionRef(String ref) {
+        return reponseEtudiantDao.deleteByReponseQuestionRef(ref);
+    }
 
-	public ReponseEtudiant save(ReponseEtudiant reponseEtudiant) {
-		Reponse reponse = this.reponseService.findReponseById(reponseEtudiant.getReponse().getId());
-		QuizEtudiant quizEtudiant = this.quizEtudiantService.findQuizEtudiantById(reponseEtudiant.getQuizEtudiant().getId());
-		Question question = this.questionService.findQuestionById(reponseEtudiant.getQuestion().getId());
-		reponseEtudiant.setReponse(reponse);
-		reponseEtudiant.setQuizEtudiant(quizEtudiant);
-		reponseEtudiant.setQuestion(question);
-		reponseEtudiant.setRef(RandomStringUtils.randomAlphanumeric(3));
-		return reponseEtudiantDao.save(reponseEtudiant);
-	}
+    @Transactional
+    public int deleteByQuizEtudiantEtudiantRef(String ref) {
+        return reponseEtudiantDao.deleteByQuizEtudiantEtudiantRef(ref);
+    }
 
-	public List<ReponseEtudiant> findAll() {
-		return reponseEtudiantDao.findAll();
-	}
+    public ReponseEtudiant save(ReponseEtudiant reponseEtudiant) {
+        Reponse reponse;
+        if (reponseEtudiant.getReponse().getId() == null) {
+            reponse = this.reponseService.saveAnswer(reponseEtudiant.getReponse());
+        } else {
+            reponse = this.reponseService.findReponseById(reponseEtudiant.getReponse().getId());
+        }
+        QuizEtudiant quizEtudiant = this.quizEtudiantService.findQuizEtudiantById(reponseEtudiant.getQuizEtudiant().getId());
+        Question question = this.questionService.findQuestionById(reponseEtudiant.getQuestion().getId());
+        reponseEtudiant.setReponse(reponse);
+        reponseEtudiant.setQuizEtudiant(quizEtudiant);
+        reponseEtudiant.setQuestion(question);
+        reponseEtudiant.setRef(RandomStringUtils.randomAlphanumeric(3));
+        return reponseEtudiantDao.save(reponseEtudiant);
+    }
+
+    public List<ReponseEtudiant> findAll() {
+        return reponseEtudiantDao.findAll();
+    }
 
 
-	public List<ReponseEtudiant> findByQuizEtudiantEtudiantId(Long id) {
-		return reponseEtudiantDao.findByQuizEtudiantEtudiantId(id);
-	}
+    public List<ReponseEtudiant> findByQuizEtudiantEtudiantId(Long id) {
+        return reponseEtudiantDao.findByQuizEtudiantEtudiantId(id);
+    }
 
-	public List<ReponseEtudiant> findByQuizEtudiantId(Long id) {
-		return reponseEtudiantDao.findByQuizEtudiantIdOrderByQuestionNumero(id);
-	}
+    public List<ReponseEtudiant> findByQuizEtudiantId(Long id) {
+        return reponseEtudiantDao.findByQuizEtudiantIdOrderByQuestionNumero(id);
+    }
 }
