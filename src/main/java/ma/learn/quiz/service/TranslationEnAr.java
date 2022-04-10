1b6257692eac2e4c3eae7c371cfc5a6d16ee907d
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2022-04-10 22:10:06 +0000

    commit-youssef: Vocabulary

--- src/main/java/ma/learn/quiz/service/TranslationEnAr.java
+++ src/main/java/ma/learn/quiz/service/TranslationEnAr.java
@@ -14,46 +14,51 @@ import java.util.List;
 @Service
 public class TranslationEnAr {
     public String TranslationResult(String texttotranslate) throws IOException {
-        Document doc = Jsoup.connect("https://translate.google.com/m?sl=en&tl=ar&hl=en&q=" +texttotranslate).get();
+        Document doc = Jsoup.connect("https://translate.google.com/m?sl=en&tl=ar&hl=en&q=" + texttotranslate).get();
         String result = doc.select("div.result-container").text();
+        System.out.println(result);
         return result;
     }
 
     public String TranslationResultEnFr(String texttotranslate) throws IOException {
-        Document doc = Jsoup.connect("https://translate.google.com/m?sl=en&tl=fr&hl=en&q=" +texttotranslate).get();
+        Document doc = Jsoup.connect("https://translate.google.com/m?sl=en&tl=fr&hl=en&q=" + texttotranslate).get();
         String result = doc.select("div.result-container").text();
         return result;
     }
 
-
-    public List<String> synonyme(String word)  throws IOException{
-        Document doc = Jsoup.connect("https://www.multitran.com/m.exe?s="+word+"&l1=1&l2=10").get();
-        Elements elements = doc.select("td.trans");
+    public List<String> synonyme(String word) throws IOException {
+        Document doc = Jsoup.connect("https://www.thesaurus.com/browse/" + word).get();
+        Elements elements = doc.select("ul.e1ccqdb60");
         List<String> listesynonymes = new ArrayList<>();
-        for (Element e:elements) {
-            Elements items = e.select("a");
-            for (Element item: items) {
+        int i = 0;
+        for (Element e : elements) {
+            Elements items = e.select("li");
+            for (Element item : items) {
+                i++;
                 listesynonymes.add(item.text());
+                if (i == 10) break;
             }
             break;
         }
         return listesynonymes;
     }
 
-    public String example(String word){
+    public String example(String word) {
         String example = null;
         try {
-            Document doc = Jsoup.connect("https://sentence.yourdictionary.com/"+word).get();
+            Document doc = Jsoup.connect("https://sentence.yourdictionary.com/" + word).get();
             Elements elements = doc.select("ul.sentences-list");
-            for (Element e:elements) {
+            for (Element e : elements) {
                 Elements items = e.select("li.sentences-list-item");
-                for (Element item: items) {
+                for (Element item : items) {
                     example = item.text();
+                    System.out.println("============example ==============");
+                    System.out.println(example);
+                    break;
                 }
-                break;
             }
 
-        }catch (IOException ioException){
+        } catch (IOException ioException) {
             System.out.println(ioException.getMessage());
             example = "WARNING : You should only set one word to get it in example";
         }
@@ -62,32 +67,43 @@ public class TranslationEnAr {
     }
 
     public String explanation(String texttoexplain) throws IOException {
-        String textwithoutspace = texttoexplain.replace(" ","-");
-        Document doc = Jsoup.connect("https://www.collinsdictionary.com/dictionary/english/" +textwithoutspace).get();
-        return doc.select("div.sense.def").first().text();
+        String textwithoutspace = texttoexplain.replace(" ", "-");
+        Document doc = Jsoup.connect("https://www.oxfordlearnersdictionaries.com/definition/english/" + textwithoutspace + "?q=" + textwithoutspace).get();
+        System.out.println(doc.select("span.def").first().text());
+        return doc.select("span.def").first().text();
+    }
+
+
+    public String getImg(String texttoexplain) throws IOException {
+        String textwithoutspace = texttoexplain.replace(" ", "-");
+        Document doc = Jsoup.connect("https://www.oxfordlearnersdictionaries.com/definition/english/" + textwithoutspace + "?q=" + textwithoutspace).get();
+        String src = doc.select("a.topic").attr("href");
+        System.out.println(src);
+        return src;
     }
 
     public SectionItem translationFeatures(String textTaped) throws IOException {
         SectionItem sectionItem = new SectionItem();
-        sectionItem.setTranslation(TranslationResult(textTaped));
+        sectionItem.setTranslation(this.TranslationResult(textTaped));
+        this.getImg(textTaped);
         sectionItem.setExample(example(textTaped));
         sectionItem.setExplanation(explanation(textTaped));
-        sectionItem.setSynonyms(synonymeInEnglish(textTaped));
+        sectionItem.setSynonyms(synonyme(textTaped));
         return sectionItem;
     }
 
-    public List<String> synonymeInEnglish(String word)  throws IOException{
-        Document doc = Jsoup.connect("https://www.collinsdictionary.com/dictionary/english-thesaurus/"+word).get();
-        Elements elements = doc.select("div.sense.def");
+    public List<String> synonymeInEnglish(String word) throws IOException {
+        Document doc = Jsoup.connect("https://www.oxfordlearnersdictionaries.com/definition/english/" + word + "?q=" + word).get();
+        Elements elements = doc.select("li.li");
         List<String> listesynonymes = new ArrayList<>();
         int i = 0;
-        for (Element e:elements) {
-            if(!e.text().startsWith(word)){
+        for (Element e : elements) {
+            if (!e.text().startsWith(word)) {
                 i++;
                 listesynonymes.add(e.text());
             }
-            if(i>=2)
-            break;
+            if (i >= 2)
+                break;
         }
         return listesynonymes;
     }
