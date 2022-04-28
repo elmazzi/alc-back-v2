8f66a7aaf1aa58e204e734d4b6965751df50d8c2
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2022-04-28 15:27:23 +0000

    server prob

--- src/main/java/ma/learn/quiz/config/CorsConfig.java
+++ src/main/java/ma/learn/quiz/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
                 registry.addMapping( "/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
-                        .allowedOrigins("http://localhost:4200", "http://54.37.9.19:4200", "http://54.37.9.19:4200/#/" );
+                        .allowedOrigins("http://localhost:4200", "http://162.19.25.141:4200", "http://162.19.25.141:4200/#/" );
             }
         };
     }
